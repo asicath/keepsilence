722793fdc44feb63ad694e2db5512e22e4336a97
Asicath <scottpaulwilde@gmail.com>
2017-07-27 01:22:19 -0700

    new tarot

---
+++ xxii/path.js
@@ -0,0 +1,11 @@
+﻿function setSize() {
+    var e1 = document.getElementById("tarot-img");
+    var h = Math.min(721, window.innerHeight - 50);
+    e1.style.height = h + "px";
+}
+
+window.onresize = function(event) {
+    setSize();
+};
+
+setSize();
