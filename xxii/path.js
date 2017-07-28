ac2e5fba8cf2502920812df77c46e9ecf1b04a54
Scott Wilde <swilde@omniretailgroup.com>
2017-07-27 17:12:57 -0700

    color buttons

--- xxii/path.js
+++ xxii/path.js
@@ -1,11 +1,68 @@
 ﻿function setSize() {
-    var e1 = document.getElementById("tarot-img");
-    var h = Math.min(721, window.innerHeight - 50);
-    e1.style.height = h + "px";
+    var e = document.getElementById("tarot-img");
+
+    var minCardHeight = 1073;
+
+
+    // modify for proportions
+    // 721x1073 down to 480
+    minCardHeight = (minCardHeight / 721) * 480;
+
+    var h = window.innerHeight - 50;
+    if (h < minCardHeight) {
+        e.style.height = h + "px";
+        e.style.width = "";
+    }
+
+    else {
+        e.style.width = "100%";
+        e.style.height = "";
+    }
 }
+
+
 
 window.onresize = function(event) {
     setSize();
 };
 
 setSize();
+
+var colors = {};
+
+colors["30"] = {
+    "1": { color: "FF6D00" },
+    "2": { color: "FEDD00" },
+    "3": { color: "FFA500" },
+    "4": { color: "ffb734", rayed: "ed1c24" }
+};
+
+
+// set the buttons
+function setColorButtons(path) {
+
+    if (!colors.hasOwnProperty(path)) return;
+
+    var o = colors[path];
+
+    setButtonColor(document.getElementById("scale-king"), o["1"]);
+    setButtonColor(document.getElementById("scale-queen"), o["2"]);
+    setButtonColor(document.getElementById("scale-emperor"), o["3"]);
+    setButtonColor(document.getElementById("scale-empress"), o["4"]);
+}
+
+function setButtonColor(el, color) {
+    el.style.backgroundColor = "#" + color.color;
+
+    if (color.hasOwnProperty("rayed")) {
+        el.style.color = "#" + color.rayed;
+    }
+
+    el.onclick = function() {setColor(color);}
+}
+
+function setColor(color) {
+    document.getElementById("body").style.backgroundColor = "#" + color.color;
+}
+
+setColorButtons(window.path);
