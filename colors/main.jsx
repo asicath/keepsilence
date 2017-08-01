395ce2a63f101b80f817b46d3efcfbe919a815ef
Scott Wilde <swilde@omniretailgroup.com>
2017-08-01 16:20:27 -0700

    page background with effects

--- colors/main.jsx
+++ colors/main.jsx
@@ -3,22 +3,14 @@
 
 class CardDisplay extends React.Component {
     render() {
+        var imgUrl = "img/" + this.props.cardId + ".jpg";
         return <table className="card-display"><tbody><tr>
-            <td className="col-text">
-                <p>
-                This card represents the element of Water in its most secret and original form. It is the feminine complement of the Ace of Wands, and is derived from the Yoni and the Moon exactly as that is from the Lingam and the Sun. The third in the Hierarchy. This accordingly represents the essential form of the Holy Grail. Upon the dark sea of Binah, the Great Mother, are Lotuses, two in one, which fill the cup with the Life-fluid, symbolically represented either as Water, as Blood, or as Wine, according to the selected purpose of the symbolism. This being a primordial card, the liquid is shown as water; it can be transformed into Wine or Blood as may be required.
-                </p><p>
-                Above the Cup, descending upon it, is the Dove of the Holy Ghost, thus consecrating the element.
-                </p><p>
-                At the base of the Cup is the Moon, for it is the virtue of this card to conceive and to produce the second form of its Nature.
-                </p>
-            </td>
-            <td className="col-card"><div className="card-title">Ace of Cups</div><img className="card" src="img/c01.jpg" /></td>
+            <td className="col-text" id="info-text">text</td>
+            <td className="col-card"><div className="card-title">{this.props.card.title}</div><img className="card" src={imgUrl} /></td>
             <td className="col-color">
-                <PathColors path={paths["3"]} />
-                <PathColors path={paths["2"]} />
-                <PathColors path={paths["23"]} />
-                <PathColors path={paths["29"]} />
+            {this.props.card.paths.map(function(pathId, i){
+                return <PathColors path={paths[pathId]} key={i} />;
+            })}
             </td>
         </tr></tbody></table>
     }
@@ -27,25 +19,76 @@ class CardDisplay extends React.Component {
 class PathColors extends React.Component {
     render() {
         return <div className="path">
-                <div className="path-title">{this.props.path.name}</div>
-                <div className="path-colors">
-                    <div><ColorButton color={this.props.path.colors[0]} /><div className="color-button-label">king scale</div></div>
-                    <div><ColorButton color={this.props.path.colors[1]} /><div className="color-button-label">queen scale</div></div>
-                    <div><ColorButton color={this.props.path.colors[2]} /><div className="color-button-label">emperor scale</div></div>
-                    <div><ColorButton color={this.props.path.colors[3]} /><div className="color-button-label">empress scale</div></div>
-                </div>
+            <div className="path-title">{this.props.path.name}</div>
+            <div className="path-colors">
+                <div><ColorButton color={this.props.path.colors[0]} /><div className="color-button-label">king scale</div></div>
+                <div><ColorButton color={this.props.path.colors[1]} /><div className="color-button-label">queen scale</div></div>
+                <div><ColorButton color={this.props.path.colors[2]} /><div className="color-button-label">emperor scale</div></div>
+                <div><ColorButton color={this.props.path.colors[3]} /><div className="color-button-label">empress scale</div></div>
             </div>
+        </div>
     }
 }
 
+var currentColor = null;
 class ColorButton extends React.Component {
     render() {
-        return <div className="color-button" style={{backgroundColor: "#" + this.props.color.back}}>
-            <div className="effectHolder"><canvas className="buttonEffectCanvas" width="88" height="88" data-color={JSON.stringify(this.props.color)}></canvas></div>
+        var color = this.props.color;
+        var click = function() {
+
+            if (color === currentColor) {
+                currentColor = null;
+                setBackground(null);
+            }
+            else {
+                setBackground(color);
+                currentColor = color;
+            }
+
+        };
+        return <div className="color-button" onClick={click} style={{backgroundColor: "#" + this.props.color.back}}>
+            <div className="effectHolder"><canvas className="buttonEffectCanvas" width="94" height="88" data-color={JSON.stringify(this.props.color)}></canvas></div>
             <div className="nameHolder"><table><tbody><tr><td className="color-name">{this.props.color.name}</td></tr></tbody></table></div>
         </div>
     }
 }
+
+
+function setBackground(color) {
+
+    var background = document.getElementById('background');
+    var canvas = document.getElementById("background-effect");
+
+    if (color === null) {
+        background.style.backgroundColor = "#ffffff";
+        clearIt(canvas);
+        return;
+    }
+
+
+    background.style.backgroundColor = "#" + color.back;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    if (color.hasOwnProperty('rayed')) rayIt(canvas, color);
+    else if (color.hasOwnProperty('flecked')) fleckIt(canvas, color);
+    else clearIt(canvas);
+
+}
+
+const cards = {
+
+    "c01": {
+        title: "Ace of Cups",
+        paths: ["2", "3", "23", "29"]
+    },
+    "d06": {
+        title: "Six of Disks",
+        paths: ["13", "16"]
+    }
+
+};
 
 const paths = {
     "2": {
@@ -58,7 +101,6 @@ const paths = {
             { back: "ffffff", flecked: ["ed1c24", "0085ca", "FEDD00"], name: "White, flecked red, blue, and yellow" }
         ]
     },
-    //							"808080    FDC3C6"
     "3": {
         type: 'Sphere',
         name: 'Binah',
@@ -77,6 +119,16 @@ const paths = {
             { back: "e8e8e8", name: "Silver" },
             { back: "A5C5D9", name: "Cold pale blue" },
             { back: "e8e8e8", rayed: "8ABAD3", name: "Silver, rayed sky blue" }
+        ]
+    },
+    "16": {
+        type: 'Zodiac',
+        name: 'Taurus',
+        colors: [
+            { back: "FF4E00", name:"Red orange" },
+            { back: "000834", name:"Deep indigo" },
+            { back: "C7B63C", name:"Deep warm olive" },
+            { back: "5c3312", name:"Rich brown" }
         ]
     },
     "23": {
@@ -117,6 +169,11 @@ function updateButtonEffects() {
 
 }
 
+function clearIt(canvas) {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 function rayIt(canvas, color) {
     var ctx = canvas.getContext('2d');
     var center = {x:canvas.width/2, y: canvas.height/2};
@@ -146,6 +203,69 @@ function rayIt(canvas, color) {
 
 function fleckIt(canvas, color) {
     var ctx = canvas.getContext('2d');
+    var colorArray = Array.isArray(color.flecked) ? color.flecked : [color.flecked];
+    var colorIndex = 0;
+
+    var size = 3;
+    var radius = 1;
+    var angle = 0;
+    var maxRadius = Math.max(canvas.width, canvas.height);
+    var center = {x:canvas.width/2, y: canvas.height/2};
+
+    var idealDistance = 8;
+    var radiusIncrPerRevolution = 10;
+
+    var maxTimes = 1000000;
+
+    while (radius < maxRadius && maxTimes > 0) {
+        // find point
+        var x = Math.cos(angle) * radius + center.x;
+        var y = Math.sin(angle) * radius + center.y;
+
+        // draw at point
+        ctx.beginPath();
+        ctx.arc(x, y, size/2, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "#" + colorArray[colorIndex++ % colorArray.length];
+        ctx.fill();
+
+        var d = idealDistance;
+        var percent = -1;
+
+        while (d > 0) {
+            // determine circumference
+            var c = Math.PI * 2 * radius;
+
+            if (d > c) {
+                var percent = 0.01;
+                radius += radiusIncrPerRevolution * percent;
+                angle += Math.PI * 2 * percent;
+
+                c = Math.PI * 2 * radius * percent;
+
+                d -= c;
+
+                // angle stays the same
+            }
+            else {
+
+                percent = d / c;
+                angle += Math.PI * 2 * percent;
+                angle = angle % (Math.PI * 2);
+                radius += radiusIncrPerRevolution * percent;
+
+                d = 0; //exit
+            }
+        }
+
+        //console.log(maxTimes + " radius:" + radius + " percent:" + percent + " circum:" + c);
+
+        maxTimes--;
+    }
+
+}
+
+function fleckItOld(canvas, color) {
+    var ctx = canvas.getContext('2d');
     var fleckCount = 100;
     var colorArray = Array.isArray(color.flecked) ? color.flecked : [color.flecked];
     var colorIndex = 0;
@@ -162,10 +282,31 @@ function fleckIt(canvas, color) {
     }
 }
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-const element = <CardDisplay />;
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
-updateButtonEffects();
+function main() {
+
+    // get the id from the url
+    var cardId = getParameterByName("id");
+
+    // get the card info
+    if (!cards.hasOwnProperty(cardId)) return;
+    var card = cards[cardId];
+
+    const element = <CardDisplay card={card} cardId={cardId} />;
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+    updateButtonEffects();
+}
+
+main();
