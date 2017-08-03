fab078612ec6be97435441377318ed8d853229a9
Scott Wilde <scottpaulwilde@gmail.com>
2017-08-02 18:53:57 -0700

    colors

--- colors/main.jsx
+++ colors/main.jsx
@@ -3,10 +3,19 @@
 
 class CardDisplay extends React.Component {
     render() {
+
+        var index = list.indexOf(this.props.cardId);
+        var nextId = this.props.cardId, prevId = this.props.cardId;
+        if (index > 0) prevId = list[index - 1];
+        if (index < list.length - 1) nextId = list[index + 1];
+
+        var prevUrl = "index.htm?id=" + prevId;
+        var nextUrl = "index.htm?id=" + nextId;
+
         var imgUrl = "img/" + this.props.cardId + ".jpg";
         return <table className="card-display"><tbody><tr>
-            <td className="col-text" id="info-text">text</td>
-            <td className="col-card"><div className="card-title">{this.props.card.title}</div><img className="card" src={imgUrl} /></td>
+            <td className="col-text" id="info-text">&nbsp;</td>
+            <td className="col-card"><div className="prev"><a href={prevUrl}>prev</a></div><div className="next"><a href={nextUrl}>next</a></div><div className="card-title">{this.props.card.title}</div><img className="card" src={imgUrl} /></td>
             <td className="col-color">
             {this.props.card.paths.map(function(pathId, i){
                 return <PathColors path={paths[pathId]} key={i} />;
@@ -21,10 +30,10 @@ class PathColors extends React.Component {
         return <div className="path">
             <div className="path-title">{this.props.path.name}</div>
             <div className="path-colors">
-                <div><ColorButton color={this.props.path.colors[0]} /><div className="color-button-label">king scale</div></div>
-                <div><ColorButton color={this.props.path.colors[1]} /><div className="color-button-label">queen scale</div></div>
-                <div><ColorButton color={this.props.path.colors[2]} /><div className="color-button-label">emperor scale</div></div>
-                <div><ColorButton color={this.props.path.colors[3]} /><div className="color-button-label">empress scale</div></div>
+                <div className="col"><ColorButton color={this.props.path.colors[0]} /><div className="color-button-label">king scale</div></div>
+                <div className="col"><ColorButton color={this.props.path.colors[1]} /><div className="color-button-label">queen scale</div></div>
+                <div className="col"><ColorButton color={this.props.path.colors[2]} /><div className="color-button-label">emperor scale</div></div>
+                <div className="col"><ColorButton color={this.props.path.colors[3]} /><div className="color-button-label">empress scale</div></div>
             </div>
         </div>
     }
@@ -73,19 +82,250 @@ function setBackground(color) {
 
     if (color.hasOwnProperty('rayed')) rayIt(canvas, color);
     else if (color.hasOwnProperty('flecked')) fleckIt(canvas, color);
+    else if (color.hasOwnProperty('quartered')) quarterIt(canvas, color);
     else clearIt(canvas);
 
 }
 
 const cards = {
 
+    "t04": {
+        title: "The Emperor",
+        paths: ["15"]
+    },
+
+    "w01": {
+        title: "Ace of Wands",
+        // Fire, Sol and the Phallus (as described in ace of swords)
+        paths: ["31", "30", "27"]
+    },
+    "w02": {
+        title: "Two of Wands",
+        paths: ["2", "27", "15"]
+    },
+    "w03": {
+        title: "Three of Wands",
+        paths: ["30", "15"]
+    },
+    "w04": {
+        title: "Four of Wands",
+        paths: ["14", "15"]
+    },
+    "w05": {
+        title: "Five of Wands",
+        paths: ["31", "32","19"]
+    },
+    "w06": {
+        title: "Six of Wands",
+        paths: ["31", "21","19"]
+    },
+    "w07": {
+        title: "Seven of Wands",
+        paths: ["31", "27","19"]
+    },
+    "w08": {
+        title: "Eight of Wands",
+        paths: ["31", "12","25"]
+    },
+    "w09": {
+        title: "Nine of Wands",
+        paths: ["31", "13", "25"]
+    },
+    "w10": {
+        title: "Ten of Wands",
+        paths: ["31", "32", "25"]
+    },
+    "w-knight": {
+        title: "knight of Wands",
+        paths: ["31"]
+    },
+    "w-queen": {
+        title: "queen of Wands",
+        paths: ["31", "23"]
+    },
+    "w-prince": {
+        title: "prince of Wands",
+        paths: ["31","11"]
+    },
+    "w-princess": {
+        title: "princess of Wands",
+        paths: ["31", "32bis"]
+    },
+    
+    
     "c01": {
         title: "Ace of Cups",
         paths: ["2", "3", "23", "29"]
     },
+    "c02": {
+        title: "Two of Cups",
+        paths: ["14", "18"]
+    },
+    "c03": {
+        title: "Three of Cups",
+        paths: ["12", "18"]
+    },
+    "c04": {
+        title: "Four of Cups",
+        paths: ["13", "18"]
+    },
+    "c05": {
+        title: "Five of Cups",
+        paths: ["27", "24"]
+    },
+    "c06": {
+        title: "Six of Cups",
+        paths: ["30", "24"]
+    },
+    "c07": {
+        title: "Seven of Cups",
+        paths: ["14", "24"]
+    },
+    "c08": {
+        title: "Eight of Cups",
+        paths: ["32", "29"]
+    },
+    "c09": {
+        title: "Nine of Cups",
+        paths: ["21", "29"]
+    },
+    "c10": {
+        title: "Ten of Cups",
+        paths: ["27", "29"]
+    },
+    "c-knight": {
+        title: "knight of Cups",
+        paths: ["23","31"]
+    },
+    "c-queen": {
+        title: "queen of Cups",
+        paths: ["23"]
+    },
+    "c-prince": {
+        title: "prince of Cups",
+        paths: ["23", "11"]
+    },
+    "c-princess": {
+        title: "princess of Cups",
+        paths: ["23", "32bis"]
+    },
+
+
+    "s01": {
+        title: "Ace of Swords",
+        paths: ["11", "6"]
+    },
+    "s02": {
+        title: "Two of Swords",
+        paths: ["13","22"]
+    },
+    "s03": {
+        title: "Three of Swords",
+        paths: ["32","22"]
+    },
+    "s04": {
+        title: "Four of Swords",
+        paths: ["21","22"]
+    },
+    "s05": {
+        title: "Five of Swords",
+        paths: ["14","28"]
+    },
+    "s06": {
+        title: "Six of Swords",
+        paths: ["12","28"]
+    },
+    "s07": {
+        title: "Seven of Swords",
+        paths: ["13","28"]
+    },
+    "s08": {
+        title: "Eight of Swords",
+        paths: ["21","17"]
+    },
+    "s09": {
+        title: "Nine of Swords",
+        paths: ["27","17"]
+    },
+    "s10": {
+        title: "Ten of Swords",
+        paths: ["30","17"]
+    },
+    "s-knight": {
+        title: "knight of Swords",
+        paths: ["11","31"]
+    },
+    "s-queen": {
+        title: "queen of Swords",
+        paths: ["11","23"]
+    },
+    "s-prince": {
+        title: "prince of Swords",
+        paths: ["11"]
+    },
+    "s-princess": {
+        title: "princess of Swords",
+        paths: ["11","32bis"]
+    },
+
+
+
+
+    "d01": {
+        title: "Ace of Disks",
+        paths: ["32bis-green"]
+    },
+    "d02": {
+        title: "Two of Disks",
+        paths: ["21","26"]
+    },
+    "d03": {
+        title: "Three of Disks",
+        paths: ["27","26"]
+    },
+    "d04": {
+        title: "Four of Disks",
+        paths: ["30","26"]
+    },
+    "d05": {
+        title: "Five of Disks",
+        paths: ["12","16"]
+    },
     "d06": {
         title: "Six of Disks",
-        paths: ["13", "16"]
+        paths: ["13","16"]
+    },
+    "d07": {
+        title: "Seven of Disks",
+        paths: ["32", "16"]
+    },
+    "d08": {
+        title: "Eight of Disks",
+        paths: ["30","20"]
+    },
+    "d09": {
+        title: "Nine of Disks",
+        paths: ["9", "14","20"]
+    },
+    "d10": {
+        title: "Ten of Disks",
+        paths: ["12","20"]
+    },
+    "d-knight": {
+        title: "knight of Disks",
+        paths: ["32bis","31"]
+    },
+    "d-queen": {
+        title: "queen of Disks",
+        paths: ["32bis", "23"]
+    },
+    "d-prince": {
+        title: "prince of Disks",
+        paths: ["32bis", "11"]
+    },
+    "d-princess": {
+        title: "princess of Disks",
+        paths: ["32bis"]
     }
 
 };
@@ -111,6 +351,47 @@ const paths = {
             { back: "808080", flecked: "FDC3C6", name: "Grey flecked pink" }
         ]
     },
+    "6": {
+        type: 'Sphere',
+        name: 'Tiphareth',
+        colors: [
+            //
+            { back: "FDC3C6", name: "Clear pink rose" },
+            { back: "FEDD00", name: "Yellow (gold)" },
+            { back: "f25330", name: "Rich salmon" },
+            { back: "FFCA1A", name: "Gold amber" }
+        ]
+    },
+    "9": {
+        type: 'Sphere',
+        name: 'Yesod',
+        colors: [
+            { back: "001489", name: "Indigo" },
+            { back: "440099", name: "Violet" },
+            { back: "330b33", name: "Very dark purple" },
+            { back: "dedb2c", flecked: "4D91C6", name: "Citrine, flecked azure" }
+        ]
+    },
+    "11": {
+        type: 'Element',
+        name: 'Air',
+        colors: [
+            { back: "fee74d", name: "Bright pale yellow" },
+            { back: "8ABAD3", name: "Sky blue" },
+            { back: "00958D", name: "Blue emerald green" },
+            { back: "00A550", flecked: "FFB000", name: "Emerald, flecked gold"}
+        ]
+    },
+    "12": {
+        type: 'Planet',
+        name: 'Mercury',
+        colors: [
+            { back: "FEDD00", name: "Yellow" },
+            { back: "BB29BB", name: "Purple" },
+            { back: "808080", name: "Grey" },
+            { back: "001489", rayed: "440099", name: "Indigo, rayed violet" }
+        ]
+    },
     "13": {
         type: 'Planet',
         name: 'Luna',
@@ -119,6 +400,26 @@ const paths = {
             { back: "e8e8e8", name: "Silver" },
             { back: "A5C5D9", name: "Cold pale blue" },
             { back: "e8e8e8", rayed: "8ABAD3", name: "Silver, rayed sky blue" }
+        ]
+    },
+    "14": {
+        type: 'Planet',
+        name: 'Venus',
+        colors: [
+            { back: "00A550", name:"Emerald Green" },
+            { back: "8ABAD3", name:"Sky blue" },
+            { back: "b2d135", name:"Early spring green" },
+            { back: "a41247", rayed: "B2E79F", name:"Bright rose or cerise, rayed pale green" }
+        ]
+    },
+    "15": {
+        type: 'Zodiac',
+        name: 'Aries',
+        colors: [
+            { back: "F2301B", name: "Scarlet" },
+            { back: "ed1c24", name: "Red" },
+            { back: "f5ac1c", name: "Brilliant flame" },
+            { back: "ff1500", name: "Glowing red" }
         ]
     },
     "16": {
@@ -131,6 +432,66 @@ const paths = {
             { back: "5c3312", name:"Rich brown" }
         ]
     },
+    "17": {
+        type: 'Zodiac',
+        name: 'Gemini',
+        colors: [
+            { back: "FF6D00", name: "Orange" },
+            { back: "cca3b1", name: "Pale Mauve" },
+            { back: "e8af36", name: "New yellow leather" },
+            { back: "907172", name: "Reddish grey inclined to mauve" }
+        ]
+    },
+    "18": {
+        type: 'Zodiac',
+        name: 'Cancer',
+        colors: [
+            { back: "ffb734", name: "Amber" },
+            { back: "800f13", name: "Maroon" },
+            { back: "d60d0a", name: "Rich bright russet" },
+            { back: "322F12", name: "Dark greenish brown" }
+        ]
+    },
+    "19": {
+        type: 'Zodiac',
+        name: 'Leo',
+        colors: [
+            { back: "E5D708", name: "Yellow, greenish" },
+            { back: "550055", name: "Deep purple" },
+            { back: "808080", name: "Grey" },
+            { back: "ff9934", name: "Reddish amber" }
+        ]
+    },
+    "20": {
+        type: 'Zodiac',
+        name: 'Virgo',
+        colors: [
+            { back: "59B934", name: "Green, yellowish" },
+            { back: "9faeaa", name: "Slate grey" },
+            { back: "558c70", name: "Green grey" },
+            { back: "981733", name: "Plum colour" }
+        ]
+    },
+    "21": {
+        type: 'Planet',
+        name: 'Jupiter',
+        colors: [
+            { back: "7f14aa", name: "Violet Purple" },
+            { back: "0085ca", name: "Blue" },
+            { back: "990099", name: "Rich purple" },
+            { back: "00a8ff", rayed:"fedd00", name: "Bright blue, rayed yellow" }
+        ]
+    },
+    "22": {
+        type: 'Zodiac',
+        name: 'Libra',
+        colors: [
+            { back: "00A550", name: "Emerald Green" },
+            { back: "0085ca", name: "Blue" },
+            { back: "008077", name: "Deep blue-green" },
+            { back: "B2E79F", name: "Pale green" }
+        ]
+    },
     "23": {
         type: 'Element',
         name: 'Water',
@@ -141,16 +502,128 @@ const paths = {
             { back: "ffffff", flecked:"BB29BB", name: "White, flecked purple, like mother-of-pearl" }
         ]
     },
+    "24": {
+        type: 'Zodiac',
+        name: 'Scorpio',
+        colors: [
+            { back: "00958d", name:"Green blue" },
+            { back: "9D7446", name:"Dull brown" },
+            { back: "211307", name:"Very dark brown" },
+            { back: "1c131b", name:"Livid indigo brown (like a black beetle)" }
+        ]
+    },
+    "25": {
+        type: 'Zodiac',
+        name: 'Sagittarius',
+        colors: [
+            { back: "0085ca", name: "Blue" },
+            { back: "FEDD00", name: "Yellow" },
+            { back: "00A550", name: "Green" },
+            { back: "003a80", name: "Dark vivid blue" }
+        ]
+    },
+    "26": {
+        type: 'Zodiac',
+        name: 'Capricornus',
+        colors: [
+            { back: "001489", name: "Indigo" },
+            { back: "000000", name: "Black" },
+            { back: "000a44", name: "Blue black" },
+            { back: "28292b", name: "Cold dark grey, approaching black" }
+        ]
+    },
+    "27": {
+        type: 'Planet',
+        name: 'Mars',
+        colors: [
+            { back: "ed2000", name: "Scarlet" },
+            { back: "ed1c24", name: "Red" },
+            { back: "c80815", name: "Venetian red" },
+            { back: "ff0000", rayed:"00A550", name: "Bright red, rayed azure or emerald" }
+        ]
+    },
+    "28": {
+        type: 'Zodiac',
+        name: 'Aquarius',
+        colors: [
+            { back: "440099", name: "Violet" },
+            { back: "8ABAD3", name: "Sky blue" },
+            { back: "ad78bd", name: "Blueish mauve" },
+            { back: "ffe8ff", name: "White tinged purple" }
+        ]
+    },
     "29": {
         type: 'Zodiac',
         name: 'Pisces',
         colors: [
-            { back: "AE0E36", name:"Crimson (ultra violet)" },
+            { back: "AE0E36", name: "Crimson (ultra violet)" },
             { back: "D8B998", flecked:"F2F2F2", name: "Buff, flecked silver-white"},
-            { back: "C08A80", name:"Light translucent pinkish brown" },
-            { back: "76826a", name:"Stone color" }
+            { back: "C08A80", name: "Light translucent pinkish brown" },
+            { back: "76826a", name: "Stone color" }
+        ]
+    },
+    "30": {
+        type: 'Planet',
+        name: 'Sol',
+        colors: [
+            { back: "FF6D00", name: 'Orange' },
+            { back: "FEDD00", name: 'Gold yellow' },
+            { back: "FFA500", name: 'Rich amber' },
+            { back: "ffb734", rayed: "ed1c24", name: 'Amber, rayed red' }
+        ]
+    },
+    "31": {
+        type: 'Element',
+        name: 'Fire',
+        colors: [
+            { back: "ff2a00", name: "Glowing orange scarlet" },
+            { back: "D9381E", name: "Vermilion" },
+            { back: "ed2000", flecked: "FFB000", name: "Scarlet, flecked gold" },
+            { back: "D9381E", flecked:["AE0E36", "00A550"], name: "Vermilion, flecked crimson and emerald" }
+        ]
+    },
+    "32": {
+        type: 'Planet',
+        name: 'Saturn',
+        colors: [
+            { back: "001489", name: "Indigo" },
+            { back: "000000", name: "Black" },
+            { back: "000a44", name: "Blue black" },
+            { back: "000000", rayed:"0085ca", name: "Black, rayed blue" }
+        ]
+    },
+    "31bis": {
+        type: 'Element',
+        name: 'Spirit',
+        colors: [
+            { back: "ffffff", name: "White merging into grey" },
+            { back: "220022", name: "Deep purple, nearly black" },
+            { back: "000000", name: "The 7 prismatic colours, the violet being outside" },
+            { back: "000000", name: "White, red, yellow, blue, black (the latter outside)" }
+        ]
+    },
+    "32bis": {
+        type: 'Element',
+        name: 'Earth',
+        colors: [
+            { back: "000000", quartered:["dedb2c", "8d8800", "731817", "000000"], name: "Emerald (Citrine, olive, russet, and black)" },
+            { back: "ffb734", name: "Amber" },
+            { back: "38200a", name: "Dark brown" },
+            { back: "000000", flecked:"FEDD00", name: "Black, flecked yellow" }
+        ]
+    },
+    "32bis-green": {
+        type: 'Element',
+        name: 'Earth (Green)',
+        colors: [
+            { back: "00A550", name: "Emerald (Citrine, olive, russet, and black)" },
+            { back: "ffb734", name: "Amber" },
+            { back: "38200a", name: "Dark brown" },
+            { back: "000000", flecked:"FEDD00", name: "Black, flecked yellow" }
         ]
     }
+    
+    
 };
 
 function updateButtonEffects() {
@@ -163,8 +636,12 @@ function updateButtonEffects() {
         //ctx.fillStyle = "#ff0000";
         //ctx.fillRect(0, 0, width, height);
 
+        if (canvas.parentElement.clientWidth !==  canvas.width) canvas.width = canvas.parentElement.clientWidth + 6;
+        if (canvas.parentElement.clientHeight !==  canvas.height) canvas.height = canvas.parentElement.clientHeight;
+
         if (color.hasOwnProperty('rayed')) rayIt(canvas, color);
         if (color.hasOwnProperty('flecked')) fleckIt(canvas, color);
+        if (color.hasOwnProperty('quartered')) quarterIt(canvas, color)
     }
 
 }
@@ -199,6 +676,51 @@ function rayIt(canvas, color) {
 
     ctx.fillStyle = "#" + color.rayed;
     ctx.fill();
+}
+
+function quarterIt(canvas, color) {
+    var ctx = canvas.getContext('2d');
+    var center = {x:canvas.width/2, y: canvas.height/2};
+
+    var w = canvas.width;
+    var h = canvas.height;
+
+
+
+
+    //top
+    ctx.beginPath();
+    ctx.moveTo(center.x, center.y);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(w, 0);
+    ctx.closePath();
+    ctx.fillStyle = "#" + color.quartered[0];
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(center.x, center.y);
+    ctx.lineTo(w, 0);
+    ctx.lineTo(w, h);
+    ctx.closePath();
+    ctx.fillStyle = "#" + color.quartered[1];
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(center.x, center.y);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(0, h);
+    ctx.closePath();
+    ctx.fillStyle = "#" + color.quartered[2];
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(center.x, center.y);
+    ctx.lineTo(0, h);
+    ctx.lineTo(w, h);
+    ctx.closePath();
+    ctx.fillStyle = "#" + color.quartered[3];
+    ctx.fill();
+
 }
 
 function fleckIt(canvas, color) {
@@ -292,6 +814,7 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+var list = [];
 function main() {
 
     // get the id from the url
@@ -301,6 +824,11 @@ function main() {
     if (!cards.hasOwnProperty(cardId)) return;
     var card = cards[cardId];
 
+    // create the list
+    for (var key in cards) {
+        list.push(key);
+    }
+
     const element = <CardDisplay card={card} cardId={cardId} />;
     ReactDOM.render(
         element,
@@ -308,5 +836,9 @@ function main() {
     );
     updateButtonEffects();
 }
+
+window.onresize = function(event) {
+    updateButtonEffects();
+};
 
 main();
